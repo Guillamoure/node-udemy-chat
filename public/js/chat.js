6e015c7e99fa0f389ca93e30f984877cbc249a30
Jack Hilscher <jhilscheriv@gmail.com>
2020-09-09 17:25:15 -0400

    lesson 160: form and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,32 +1,48 @@
 const socket = io()
 
+let form = document.getElementById("form")
+let formInput = form.querySelector("input")
+let formBtn = form.querySelector("button")
+let locationBtn = document.getElementById("send-location")
+
 socket.on('message', (data) => {
 	console.log(data)
 })
 
-let form = document.getElementById("form")
 
 form.addEventListener('submit', e => {
 	e.preventDefault()
 
+	 formBtn.setAttribute('disabled', 'disabled')
+
 	let text = form.message.value
 	socket.emit("sendMessage", text, (error) => {
+		formBtn.removeAttribute('disabled')
+		formInput.value = ""
+		formInput.focus()
+
 		if (error){return console.log(error)}
+
 		console.log("The message was delivered!")
 	})
 })
 
-let locationBtn = document.getElementById("send-location")
 
 locationBtn.addEventListener("click", () => {
 	if (!navigator.geolocation) {
 		return alert('Geolocation is not supported by your browser.')
 	}
+	locationBtn.setAttribute('disabled', 'disabled')
+
 	navigator.geolocation.getCurrentPosition((position) => {
+
 		socket.emit("sendLocation", {
 			latitude: position.coords.latitude,
 			longitude: position.coords.longitude
 		}, (confirmation) => {
+
+			locationBtn.removeAttribute('disabled')
+			
 			console.log(confirmation)
 		})
 	})
